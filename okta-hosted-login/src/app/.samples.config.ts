954937e845fc73cd28641f7f306e7a254da17c73
Robert Campbell <robertgcampbell95@gmail.com>
2018-12-04 15:34:37 -0800

    Update scope to include groups

--- okta-hosted-login/src/app/.samples.config.ts
+++ okta-hosted-login/src/app/.samples.config.ts
@@ -3,7 +3,7 @@ export default {
     clientId: '0oai1gohhtcxbBjLv0h7',
     issuer: 'https://dev-323697.oktapreview.com/oauth2/default',
     redirectUri: 'http://104.248.178.198:4200/implicit/callback',
-    scope: 'openid profile email',
+    scope: 'openid profile email groups',
   },
   resourceServer: {
     messagesUrl: 'http://localhost:8000/api/messages',
